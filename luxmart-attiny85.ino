e1e2573518a4c2b01899d5a43d3f875e975b1c40
Dmitry Mikushin <dmitry@kernelgen.org>
2023-07-08 22:14:10 +0200

    Let's keep the classic step-based commands, as well as minimum/maximum commands. Simplifying the direct value specification command

--- luxmart-attiny85.ino
+++ luxmart-attiny85.ino
@@ -6,38 +6,55 @@
 #define TX_PIN 3
 
 int value = 0;
+const int step = 10;
+
 SoftSerial serial(RX_PIN, TX_PIN);
 
 void setup()
 {
-    serial.begin(9600);
-    pinMode(PWM_PIN, OUTPUT);
-    analogWrite(PWM_PIN, value);
+  serial.begin(9600);
+  pinMode(PWM_PIN, OUTPUT);
+  analogWrite(PWM_PIN, value);
 }
 
 void loop()
 {
-    if (serial.available())
+  if (serial.available())
+  {
+    char rc = serial.read();
+    switch (rc)
     {
-        char rc = serial.read();
-        switch (rc)
-        {
-            case 'w':
-            value += step;
-            value = min(255, value);
-            analogWrite(PWM_PIN, value);
-
-            int newValue = serial.read();
-            if(0 < newValue && newValue < 255)
-                value = newValue;
-
-            analogWrite(PWM_PIN, value);
-            break;
-
-            case 'r':
-            serial.write('w');
-            serial.write(value);
-            break;
-        }
+      // Keys for incrementing and decrementing by a fixed step
+    case 'u' :
+      value += step;
+      value = min(255, value);
+      analogWrite(PWM_PIN, value);
+      break;
+    case 'd' :
+      value -= step;
+      value = max(0, value);
+      analogWrite(PWM_PIN, value);
+      break;
+      // Key for setting a specific value
+    case 'w':
+      value = serial.read();
+      value = *(reinterpret_cast<unsigned char*>(&value));
+      analogWrite(PWM_PIN, value);
+      break;
+      // Key for reading the value
+    case 'r':
+      serial.write('w');
+      serial.write(value);
+      break;
+      // Keys for setting to minimum and maximum value
+    case 'm' :
+      value = 255;
+      analogWrite(PWM_PIN, value);
+      break;
+    case '0' :
+      value = 0;
+      analogWrite(PWM_PIN, value);
+      break;
     }
+  }
 }
