5ebd46577c4695c41279f4017c430a67ebec4ce3
Dmitry Mikushin <dmitry@kernelgen.org>
2023-07-09 14:27:58 +0200

    Getting the test to finish when a reasonably good state is reached

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,10 @@
+// This test is checking the ATTiny85 firmware on a development machine.
+// We create a minimum environment for the firmware to emulate the serial
+// communication.
+
 #include "luxmart-attiny85.ino"
 
+#include <atomic>
 #include <chrono>
 #include <iostream>
 #include <thread>
@@ -19,13 +24,17 @@ const std::string putCommands[] =
 	"r"
 };
 
+const char getValues[] = { '\x0', '\x2a' };
+
 int main(int argc, char* argv[])
 {
-	std::thread loopThread([]()
+	std::atomic<bool> shutdown = false;
+
+	std::thread loopThread([&shutdown]()
 	{
 		setup();
 
-		while (1)
+		while (!shutdown)
 		{
 			loop();
 			std::this_thread::sleep_for(std::chrono::milliseconds(10));
@@ -46,6 +55,8 @@ int main(int argc, char* argv[])
 
 	std::thread serialGetThread([]()
 	{
+		unsigned int ivalue = 0;
+
 		while (1)
 		{
 			char ch;
@@ -54,15 +65,33 @@ int main(int argc, char* argv[])
 				if (ch == 'v')
 					std::cout << 'v';
 				else
+				{					
+					auto ch_expected = getValues[ivalue++];
+					if (ch != ch_expected)
+					{
+						std::cerr << "Unexpected output value: got " << hex << static_cast<int>(ch) <<
+							", expected " << hex << static_cast<int>(ch_expected) << std::endl;
+						exit(-2);
+					}
+					
 					std::cout << "0x" << hex << static_cast<int>(ch) << std::endl;
+
+					if (ivalue >= sizeof(getValues)) return;
+				}
 			}
+
 			std::this_thread::sleep_for(std::chrono::milliseconds(10));
 		}
 	});
 
-	loopThread.join();
+	// First wait for all inputs to be consumed.
 	serialPutThread.join();
-	serialGetThread.join();
 
+	// Next, wait for all expected output.
+	serialGetThread.join();
+	
+	// Finally, shut down the loop.
+	shutdown = true;
+	loopThread.join();
 	return 0;
 }
