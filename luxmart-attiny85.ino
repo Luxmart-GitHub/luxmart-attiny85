a574b6aece4f449e6db0c7a1c189c160644d27ac
Dmitry Mikushin <dmitry@kernelgen.org>
2023-07-12 00:32:14 +0200

    Working on the button and led support

--- luxmart-attiny85.ino
+++ luxmart-attiny85.ino
@@ -3,7 +3,9 @@
 
 #include "api/pwm_controller_commands.h"
 
+#define LED_PIN 0
 #define PWM_PIN 4
+#define BTN_PIN 2
 #define RX_PIN 1
 #define TX_PIN 3
 
@@ -26,13 +28,91 @@ static SoftSerial serial(RX_PIN, TX_PIN);
 
 void setup()
 {
-	serial.begin(BAUD_RATE);
-	pinMode(PWM_PIN, OUTPUT);
-	analogWrite(PWM_PIN, value);
+  serial.begin(BAUD_RATE);
+  pinMode(PWM_PIN, OUTPUT);
+  analogWrite(PWM_PIN, value);
 	
-	// TODO Button pin mode
-	
-	// TODO LED1 pin mode
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BTN_PIN, INPUT_PULLUP);
+}
+
+static int blink_count = -1;
+static int blink_interval = 0;
+
+static void process_blink()
+{
+  static char state = LOW;
+  static unsigned long prev_blink = millis();
+
+  if ((blink_interval == 0) || (blink_count == 0)) return;
+
+  if (millis() > prev_blink + blink_interval)
+  {
+    digitalWrite(LED_PIN, state);
+    state ^= HIGH;
+    if (blink_count > 0) blink_count -= state;
+  }
+}
+
+static void process_button()
+{
+  // TODO
+  //digitalRead(BTN_PIN); // HIGH or LOW
+}
+
+static void receive_value(char value)
+{
+  analogWrite(PWM_PIN, *(reinterpret_cast<unsigned char*>(&value)));
+}
+
+void (*callback)(char);
+
+static void receive_blink_mode(char mode)
+{
+  // TODO Use ^XOR blinker
+  switch(mode)
+  {
+  case BlinkOff :
+    blink_interval = 0;
+    digitalWrite(LED_PIN, HIGH);
+    break;
+  case BlinkOn :
+    blink_interval = 0;
+    digitalWrite(LED_PIN, LOW);
+    break;
+  case BlinkSlow1s :
+    blink_interval = 1000;
+    blink_count = 1;
+    break;
+  case BlinkSlow2s :
+    blink_interval = 1000;
+    blink_count = 2;
+    break;
+  case BlinkSlow3s :
+    blink_interval = 1000;
+    blink_count = 3;
+    break;
+  case BlinkSlowInfinite :
+    blink_interval = 1000;
+    blink_count = -1;
+    break;
+  case BlinkFast1 :
+    blink_interval = 500;
+    blink_count = 1;
+    break;
+  case BlinkFast2 :
+    blink_interval = 500;
+    blink_count = 2;
+    break;
+  case BlinkFast3 :
+    blink_interval = 500;
+    blink_count = 3;
+    break;
+  case BlinkFastInfinite :
+    blink_interval = 500;
+    blink_count = -1;
+    break;
+  }
 }
 
 void loop()
@@ -60,6 +140,8 @@ void loop()
 			case CommandSetValue :
 				// Switch serial interpretation to value mode.
 				mode = ReceiveValue;
+        // Handle the received value with appropriate callback.
+        callback = &receive_value;
 				break;
 				// Key for reading the value
 			case CommandGetValue :
@@ -77,7 +159,10 @@ void loop()
 				analogWrite(PWM_PIN, value);
 				break;
 			case CommandBlink :
-				// TODO
+				// Switch serial interpretation to value mode.
+				mode = ReceiveValue;
+        // Handle the received value with appropriate callback.
+        callback = &receive_blink_mode;
 				break;
 			case CommandReboot :
 				// TODO
@@ -88,11 +173,13 @@ void loop()
 			}
 			break;
 		case ReceiveValue:
-			value = *(reinterpret_cast<unsigned char*>(&rc));
-			analogWrite(PWM_PIN, value);
+			callback(rc);
 			// Value received; switc serial back to command mode.
 			mode = ReceiveCommand;
 			break;
 		}
 	}
+
+  process_blink();
+  process_button();
 }
